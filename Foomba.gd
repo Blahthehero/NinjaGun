e9e3762ef5461b674af05665f309210a0f6ff7e3
Blahthehero <91135952+Blahthehero@users.noreply.github.com>
2022-01-18 13:48:40 -0500

    Add files via upload

--- Foomba.gd
+++ Foomba.gd
@@ -3,21 +3,23 @@ var GRAVITY = 50
 var MOVE = 5
 onready var PlayPos = get_node("/root/YLP?/Player")
 var canShoot = true
-const Dagger = preload("res://Scenes/Dagger.tscn")
+onready var Dagger = preload("res://Scenes/Dagger.tscn")
 onready var Bullet = get_node("/root/YLP?/Ammo/Bullet")
 onready var Player = get_node("/root/YLP?/Player/Play")
 var ableshot = false
 
 func shoot():
 	var muzzle = $Muzzle
-	var instance = Dagger.instance()
-	get_node("/root/YLP?").add_child(instance) 
-	instance.global_position = muzzle.global_position
-	canShoot = false
+	var Knife = Dagger.instance()
+	get_node("/root/YLP?").call_deferred("add_child",Knife) 
+	Knife.global_position = muzzle.global_position
+	var dir = (Player.global_position - global_position).normalized()
+	Knife.global_rotation = dir.angle()
+	Knife.direction = dir
 
 func _on_Area2D_area_entered(area):
 	if area.get_name() == 'Bullet': 
-		queue_free()
+		self.queue_free()
 
 
 func _on_Area2D_body_entered(body):
@@ -31,13 +33,4 @@ func _on_Area2D_body_entered(body):
 
 func _on_Area2D2_body_entered(body):
 	if body.get_name() == 'Play':
-		ableshot == true
-
-func _process(_delta):
-	if ableshot == true:
 		shoot()
-		canShoot = false
-		print("ajsdvjbdjvb")
-	else:
-			yield(get_tree().create_timer(2), "timeout")
-			canShoot = true
