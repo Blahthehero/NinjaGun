87775b74be5700238e0f7e83c5b7baf91282d5b2
Blahthehero <91135952+Blahthehero@users.noreply.github.com>
2022-01-18 10:38:53 -0500

    Add files via upload

--- Scenes/Dagger.gd
+++ Scenes/Dagger.gd
@@ -3,15 +3,11 @@ extends Node2D
 var move = Vector2.ZERO
 var look_vec = Vector2.ZERO
 onready var player = get_node("/root/YLP?/Player/Play")
-var speed = -750
+var speed : float = 500
+var direction : Vector2 = Vector2.LEFT
 
-func _ready():
-	look_vec = player.global_position - global_position
-
-func _physics_process(delta):
-
-	position += transform.x * speed * delta
-
+func _process(delta):
+	translate(direction*speed*delta)
 
 func _on_Dagger_body_entered(body):
 	if body.get_name() == 'Play':
