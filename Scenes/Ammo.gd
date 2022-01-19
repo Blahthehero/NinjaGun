999ea349559776d27119458f754a0342e536ba62
Blahthehero <91135952+Blahthehero@users.noreply.github.com>
2022-01-19 11:19:41 -0500

    Add files via upload

--- Scenes/Ammo.gd
+++ Scenes/Ammo.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
+var move = Vector2.ZERO
+var look_vec = Vector2.ZERO
+onready var player = get_node("/root/YLP?/Player/Play")
+var speed : float = 750
+var direction : Vector2 = Vector2()
 
+func _process(delta):
+	translate(direction*speed*delta)
 
 func _on_Bullet_area_entered(area):
 	if area.get_name() == 'Hit_box':
--- bullet.gd
+++ bullet.gd
@@ -3,4 +3,4 @@ var speed = 750
 
 
 func _physics_process(delta):
-	position += transform.x * speed * delta
+	translate(speed*delta)
